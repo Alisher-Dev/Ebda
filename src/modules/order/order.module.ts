6ad14948ca6f629184c3b9a5c91666cec3fa195d
Alisher1221-ru <kr4054658@gmail.com>
2024-07-11 17:40:20 +0300

    module imports :)

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -3,9 +3,10 @@ import { OrderService } from "./order.service";
 import { OrderController } from "./order.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Order } from "./entities/order.entity";
+import { Product } from "../product/entities/product.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [TypeOrmModule.forFeature([Order, Product])],
   controllers: [OrderController],
   providers: [OrderService],
 })
